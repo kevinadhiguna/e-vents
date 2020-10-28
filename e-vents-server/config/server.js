4946834b6c780495ae8865961dd153673fc0ec97
kevinadhiguna <kevin26798@gmail.com>
2020-10-29 05:35:41 +0700

    build: release v0.1

---
+++ e-vents-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b544d9bca3b34543bf64042bb39bf8d8'),
+    },
+  },
+});
---
+++ e-vents-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fac8b650-18c6-41fe-992f-b5735e002328'
+};
